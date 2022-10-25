23a73838137874c7ad049ce17f5d575294f193b5
zhixuan <jinzhixuan666@gmail.com>
2022-10-25 11:04:26 +0000

    add mp4 type for react to handle

--- pages/timer.tsx
+++ pages/timer.tsx
@@ -1,9 +1,18 @@
+import { useEffect, useRef } from 'react';
+import video from '../public/video.mp4'
 
 const Timer = () => {
 
+    const videoRef = useRef<HTMLVideoElement>(null);
+    useEffect(() => {
+        videoRef.current?.play();
+    }, []);
+
     return (
-        <h1>Timer</h1>
-    )
+        <video controls muted ref={videoRef} >
+            <source src={video} type="video/mp4" />
+            <p>Your browser doesn{"'"}t support HTML5 video.</p>
+        </video>)
 }
 
-export default Timer
+export default Timer;
