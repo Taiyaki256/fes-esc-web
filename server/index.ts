312910d553fdd2002b8b5effc988a1303b0c4d8e
zhixuan <jinzhixuan666@gmail.com>
2022-10-26 07:10:14 +0000

    add socket server

--- server/index.ts
+++ server/index.ts
@@ -13,6 +13,97 @@ const io = new Server(8080, {
 
 console.log("Server started on port 8080");
 
+// for root setting
+let path: number = 0;
+let timerLong: number = 10000;
+
+type Game = {
+  page: number;
+  answer: string;
+  startTime: Date;
+  timerLong: number;
+};
+
+type Answer = {
+  answer: string;
+  socketId: string;
+};
+
+const Game = {
+  page: 0,
+  answer: "",
+  startTime: new Date(),
+  timerLong: 10000,
+};
+
+// a namespace for timer
+const timer = io.of("/timer");
+timer.on("connection", (socket) => {
+  console.log("timer connected");
+});
+
+// timer.emit("start", {
+//   page: nowPage,
+//   answer: nowAnswer,
+//   startTime: startTime,
+//   timerLong: timerLong,
+// });
+
+// a dynamic namespace
+// ex: /q-{path}
+
+const question = io.of(/^\/q-\d+$/);
+question.on("connection", (socket) => {
+  console.log("connected");
+  const namespace = socket.nsp;
+  if (namespace.name === "/q-" + path.toString()) {
+    timer.emit("timerStart", true);
+
+    socket.on("answer", (data: string) => {
+      Game.answer = data;
+      const rep: Answer = {
+        answer: data,
+        socketId: socket.id,
+      };
+      namespace.emit("answer", rep);
+    });
+    socket.on("check", () => {
+      socket.emit("check", true);
+    });
+    socket.on("next", (data: number) => {
+      Game.page = data;
+      namespace.emit("next", {
+        page: Game.page,
+      });
+    });
+  }
+});
+
+// a namespace for the dashboard
+// ex: /dashboard
+// method:
+//  - reset
+//  - start
+
+const dashboard = io.of("/dashboard");
+dashboard.on("connection", (socket) => {
+  console.log("connected");
+  const namespace = socket.nsp;
+  namespace.on("reset", () => {
+    Game.page = 0;
+    Game.answer = "";
+    Game.timerLong = 10000;
+    timer.emit("reset", true);
+  });
+  namespace.on("start", () => {
+    Game.page = 0;
+    Game.answer = "";
+    Game.timerLong = 10000;
+    Game.startTime = new Date();
+    timer.emit("start", true);
+  });
+});
+
 io.on("connection", (socket) => {
   // ...
 });
