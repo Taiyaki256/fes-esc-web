7463ebddbd3587b4266f14388dc13db20f769c2b
zhixuan <jinzhixuan666@gmail.com>
2022-10-26 07:56:57 +0000

    git push

--- pages/timer.tsx
+++ pages/timer.tsx
@@ -1,10 +1,36 @@
 import MyTimer from 'components/timer';
+import router from 'next/router';
 import { useEffect, useRef, useState } from 'react';
+import { useTimer } from 'react-timer-hook';
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000/timer");
+
 
 const TimerPage = () => {
     const [isEnd, setEnd] = useState(false);
-    const [time, setTime] = useState(new Date());
+    const [time, setTime] = useState<Date>(new Date());
 
+    useEffect(() => {
+        socket.on("connect", () => {
+            console.log("connected");
+        });
+        socket.on("disconnect", () => {
+            console.log("disconnected");
+        });
+        socket.on("reset", () => {
+            router.reload()
+        })
+        socket.on("start", () => {
+            // set time to 5 minutes
+            const time = new Date();
+            time.setSeconds(time.getSeconds() + 300);
+            setTime(time);
+        })
+        socket.on("timerStart", () => {
+            restart(time);
+        })
+    });
     const videoRef = useRef<HTMLVideoElement>(null);
     useEffect(() => {
         videoRef.current?.play();
@@ -20,13 +46,31 @@ const TimerPage = () => {
         });
 
     }, []);
+    const {
+        seconds,
+        minutes,
+        hours,
+        days,
+        isRunning,
+        start,
+        pause,
+        resume,
+        restart,
+    } = useTimer({ expiryTimestamp: time, onExpire: () => console.warn('onExpire called') });
+
 
     return (
         <>
             <div className="w-screen h-screen">
                 {isEnd ? (
                     <div className="w-screen h-screen bg-black flex justify-center items-center">
-                        <MyTimer expiryTimestamp={time} />
+                        <div style={{ textAlign: 'center' }}>
+                            <div style={{ fontSize: '100px' }}>
+                                <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+                            </div>
+                            {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
+
+                        </div>
                     </div>
                 ) : (
                     <video className="w-screen h-screen" controls muted ref={videoRef} >
