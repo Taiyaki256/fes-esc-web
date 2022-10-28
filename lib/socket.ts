7e3cdd611683be21cec355a9a0132cefc3b87a1b
zhixuan <zhixuan@zhixuan.dev>
2022-10-28 10:08:36 +0900

    update

--- lib/socket.ts
+++ lib/socket.ts
@@ -21,7 +21,7 @@ const URL =
     ? "http://localhost:3000/"
     : "https://fes-esc-web.vercel.app/";
 
-const wsURL = "https://89b2-138-2-46-153.jp.ngrok.io/";
+const wsURL = "https://3220-138-2-46-153.jp.ngrok.io/";
 // process.env.NODE_ENV !== "production"
 //   ? "https://0.tcp.jp.ngrok.io:17544"
 //   : "https://ws.fes.demo.zhixuan.dev/";
